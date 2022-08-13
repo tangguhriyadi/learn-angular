9ce4e5963489362df6dd38f3db0c700506c1588f
tangguhriyadi <mtangguh97@gmail.com>
2022-08-13 08:07:02 +0700

    create 1st component in angular !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { firstComp } from './first-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    firstComp
   ],
   imports: [
     BrowserModule
---
+++ src/app/first-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-comp',
+  template: `
+    <h2>Hello Tangguh !</h2>
+    <p>This is my first component!</p>
+  `
+})
+export class firstComp {
+  // The code in this class drives the component's behavior.
+}
